08c4d33e17b4f37de2818f965724c45dce1c1111
Askhat Zhamabayev <jam.askhat@gmail.com>
2021-11-14 17:54:07 +0600

    feat: add pipeline

--- index.mjs
+++ index.mjs
@@ -1,23 +1,58 @@
 import fs from 'fs';
-import { Readable, Writable } from 'stream';
+import { pipeline } from 'stream';
+
+import { Read, Write } from './lib/streams.mjs';
 
 import options from './lib/getProps.mjs';
 const {errorArg, config, input, output} = options;
 
-import { checkConfig } from './lib/checkConfig.mjs';
+import { checkConfig, checkExistFile } from './lib/check.mjs';
 
 if (errorArg) {
-  console.error('Error message: ' + errorArg);
+  process.stderr.write('Error message: ', errorArg);
   process.exit(1);
 }
 
 if (!config) {
-  console.error('Need "-c" argument!');
+  process.stderr.write('Error message: Need "-c" argument!');
   process.exit(1);
 }
 
-const {configError, configArr} = checkConfig(config);
+if (!checkConfig(config)) {
+  process.stderr.write('Error message: wrong config argument!');
+  process.exit(1);
+}
+
+let readStream;
+if (input) {
+  if (!checkExistFile(input)) {
+    process.stderr.write('Error message: Input file does not exist!');
+    process.exit(1);
+  }
+  readStream = new Read(input);
+} else {
+  readStream = process.stdin;
+}
+
+let writeStream;
+if (output) {
+  if (!checkExistFile(output)) {
+    process.stderr.write('Error message: Output file does not exist!');
+    process.exit(1);
+  }
+  writeStream = new Write(output);
+} else {
+  writeStream = process.stdout;
+}
+
+pipeline(
+  readStream,
+  writeStream,
+  (err) => {
+    if (err) process.stderr.write('Error message: ', err);
+  }
+)
+
 
 
 console.log(config, input, output, errorArg);
-console.log(configError, configArr)
